9bd216d5361d468d6b08ecdbf2df55eca96389c5
xxfaidxxx <faidwijaya@gmail.com>
2025-02-15 23:27:53 +0700

    Admin Panel Setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminMainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'rolemanager:customer'])->name('dashboard');
 
+// admin routes
+Route::middleware(['auth', 'verified', 'rolemanager:admin'])->group(function () {
+    Route::controller(AdminMainController::class)->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::get('/dashboard', 'index')->name('admin');
+        });
+    });
+});
 Route::get('/admin/dashboard', function () {
     return view('admin');
 })->middleware(['auth', 'verified', 'rolemanager:admin'])->name('admin');
