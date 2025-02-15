e9bc2b47b414bfddb5e9ff4c56d49adf9125f7ee
xxfaidxxx <faidwijaya@gmail.com>
2025-02-15 13:07:57 +0700

    create routes for admin and vendor

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin/dashboard', function () {
+    return view('admin');
+})->middleware(['auth', 'verified'])->name('admin');
+
+Route::get('/vendor/dashboard', function () {
+    return view('vendor');
+})->middleware(['auth', 'verified'])->name('vendor');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
